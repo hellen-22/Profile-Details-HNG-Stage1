23651f2d6e342b29b48a74330fc44d01b7d58e84
Hellen Wainaina <hellenwain@gmail.com>
2022-11-01 08:30:51 +0300

    adding links

--- profile/src/components/Links.js
+++ profile/src/components/Links.js
@@ -5,16 +5,19 @@ function Links() {
   return (
     <div className='links'>
         <div className='row'>
-            <button className='btn btn-primary'><a>Zuri</a></button>
+            <a href='https://training.zuri.team/' target='_blank' rel="noreferrer"><button className='btn btn-primary' id='btn__zuri'>Zuri</button></a>
         </div>
         <div className='row'>
-            <button className='btn btn-primary'><a>Zuri</a></button>
+            <a href='http://books.zuri.team' target='_blank' rel="noreferrer"><button className='btn btn-primary' id='books'>Design and Coding Books</button></a>
         </div>
         <div className='row'>
-            <button className='btn btn-primary'><a>Zuri</a></button>
+            <a href='https://books.zuri.team/python-for-beginners?ref_id=HellenWainaina' target='_blank' rel="noreferrer"><button className='btn btn-primary' id='book__python'>Python Book</button></a>
         </div>
         <div className='row'>
-            <button className='btn btn-primary'><a>Zuri</a></button>
+            <a href='https://background.zuri.team' target='_blank' rel="noreferrer"><button className='btn btn-primary' id='pitch'>Background Checker</button></a>
+        </div>
+        <div className='row'>
+            <a href='https://books.zuri.team/design-rules' target='_blank' rel="noreferrer"><button className='btn btn-primary' id='book__design'>Free Design Book</button></a>
         </div>
     </div>
   )
--- profile/src/components/Profile.js
+++ profile/src/components/Profile.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import ProfileImage from './../images/image.png'
+import ProfileImage from './../images/pic.jpeg'
 import './../css/Profile.css'
 
 function Profile() {
   return (
     <div className='profile'>
-      <img src={ProfileImage} alt='Profile' id='profile__img' className='img-thumbnail rounded-circle mb-3' style={{width: 150, height:150}}/>
+      <img src={ProfileImage} alt='Profile' id='profile__img' className='rounded-circle mb-3' style={{width: 150, height:150}}/>
       <h5 id='twitter'>@wainainaHellen_</h5>
       <h5 id='slack' hidden>Hellen Wainaina</h5>
     </div>
