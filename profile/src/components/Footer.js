6a5e49b963f2ffc3f07253cdc2c1e13576c0fc0f
Hellen Wainaina <hellenwain@gmail.com>
2022-11-03 19:30:29 +0300

    Adding social media details

--- profile/src/components/Footer.js
+++ profile/src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './../css/Footer.css'
+import Zuri from './../images/zuri.jpeg'
+import i4g from './../images/i4g.jpg'
 
 function Footer() {
   return (
@@ -7,13 +9,13 @@ function Footer() {
         <hr></hr>
         <div className='row'>
             <div className='col-md-4 zuri'>
-                <p>Zuri Internship</p>
+                <img src={Zuri} style={{width: 150, height:20}}/>
             </div>
             <div className='col-md-4 hng'>
                 <p>HNG Internship 9 Frontend</p>
             </div>
             <div className='col-md-4 i4g'>
-                <p>Ingressive for good</p>
+                <img src={i4g} style={{width: 100, height:20}}/>
             </div>
             
             
--- profile/src/components/Socials.js
+++ profile/src/components/Socials.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import "./../css/Socials.css"
+import { FaGithub, FaSlack } from 'react-icons/fa'
+import Slack from './../images/slack.jpg'
+import Github from './../images/github.png'
+import "./../css/Socials.css"
 
 function Socials() {
   return (
     <div className='container'>
-        <div className='row'>
-            
-        </div>
+            <div className='d-flex justify-content-center'>
+                <div className='slack'>
+                    <a href='#'><img src={Slack} style={{width: 40, height:40}}/></a>
+                </div>
+                <div className='github'>
+                    <a href='https://github.com/hellen-22'><img src={Github} style={{width: 40, height:40}} /></a>
+                </div>
+                
+            </div>
     </div>
   )
 }
